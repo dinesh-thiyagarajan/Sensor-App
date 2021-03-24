fa2a136e1399847c9716f21dd7d72166b5e3dc82
Dinesh <dineshkarthie@gmail.com>
2021-03-24 16:15:55 +0530

    Adding hilt dependency injection.

--- app/src/main/java/com/dineshworkspace/sensorapp/SensorApp.kt
+++ app/src/main/java/com/dineshworkspace/sensorapp/SensorApp.kt
@@ -2,7 +2,9 @@ package com.dineshworkspace.sensorapp
 
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class SensorApp : Application() {
 
     override fun onCreate() {
