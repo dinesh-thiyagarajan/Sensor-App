a9a878d7c8526553c631b36809974d237f5640b0
Dinesh <dineshkarthie@gmail.com>
2021-03-24 16:03:55 +0530

    Adding application class

--- app/src/main/java/com/dineshworkspace/sensorapp/SensorApp.kt
+++ app/src/main/java/com/dineshworkspace/sensorapp/SensorApp.kt
@@ -1,6 +1,17 @@
 package com.dineshworkspace.sensorapp
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 
 class SensorApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        application = this
+    }
+
+    companion object {
+        lateinit var application: SensorApp
+    }
 }
